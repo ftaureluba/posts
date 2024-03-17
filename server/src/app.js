816b4b5001880ef35727bfb400cbd4628565ce87
feli <ftaurel@fi.uba.ar>
2024-03-16 23:24:34 -0300

    bien conectado al mongoDB

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const mongoDBURL = require('./config')
+const config = require('./config')
 const mongoose = require('mongoose')
-
+const {modelo} = require('./config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -27,9 +27,13 @@ app.get('/posts', (req, res) => {
 
 app.listen(process.env.PORT || 8081)
 
-mongoose.connect(mongoDBURL.mongoDBURL)
+mongoose.connect(config.mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() =>{
   console.log('App conectada')
+  modelo.find({}).then(docs => {
+    console.log('Rutinas encontradas:', docs)
+  })
+  
 }
 ).catch((error) => {console.log(error)}
 )
--- server/src/config.js
+++ server/src/config.js
@@ -1,3 +1,16 @@
-module.exports = { 
-    mongoDBURL : 'mongodb+srv://admin:admin@fitness-app.wqetgeo.mongodb.net/Fitness-App?retryWrites=true&w=majority&appName=Fitness-App'
+const { default: mongoose } = require("mongoose");
+
+
+
+const Schema = mongoose.Schema;
+
+const rutina = new Schema({
+    'Rutina': String,
+    "Descripcion": String
+})
+const modelo = mongoose.model('modelo', rutina)
+
+module.exports = {
+    mongoDBURL : 'mongodb+srv://admin:admin@fitness-app.wqetgeo.mongodb.net/Fitness-App?retryWrites=true&w=majority&appName=Fitness-App',
+    modelo:modelo
 }
