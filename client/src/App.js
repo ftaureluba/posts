d9b291457d8409c0fb7ffb646fcc4a4d895572a7
feli <ftaurel@fi.uba.ar>
2024-03-23 06:56:37 -0300

    Agregue Navbar(falta personalizacion)

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Posts from './components/Posts';
-
+import NavbarComponent from './components/NavbarComponent.jsx'
 import RutinaDetailContainer from './components/RutinaDetailContainer'
 import Home from './components/Home';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path="/posts" element={<Posts />} />
-      <Route path='/:rutina_id/' element = {<RutinaDetailContainer />}/>
-    </Routes>
+    <div className='App'>
+      <NavbarComponent />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path='/:rutina_id/' element = {<RutinaDetailContainer />}/>
+      </Routes>
+    </div>
   );
 }
 
