a81b54e02f2897647deb19e65babe7d6e84d130e
feli <ftaurel@fi.uba.ar>
2024-05-26 23:55:08 -0300

    styling masomenos bien

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,22 @@ import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import { AuthProvider } from './components/AuthContext.jsx';
 import Historial from './components/Historial.jsx';
-
+import './App.css'
 function App() {
   return (
     <AuthProvider>
       <div className='App'>
         <NavbarComponent />
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path='/:rutina_id/' element = {<RutinaDetailContainer />}/>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/historial' element = {<Historial />} />
-        </Routes>
+        <div className='content'>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path='/:rutina_id/' element = {<RutinaDetailContainer />}/>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/historial' element = {<Historial />} />
+          </Routes>
+          </div>
       </div>
     </AuthProvider>
   );
--- client/src/components/Rutina.jsx
+++ client/src/components/Rutina.jsx
@@ -3,14 +3,17 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { CardActionArea, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import '../styles/rutinaCard.css'
 const Rutina = ({rutina}) => {
 
   const rutinaID = rutina._id.toString();
   const ejercicios = rutina.ejercicios
+  
   return (
     
     <Card sx={{
       maxWidth: 345,
+
       height: '100%',
       display: 'flex',
       flexDirection: 'column',
