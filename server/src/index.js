2a2b9f505d00292d754b2f33babd9ccea80f9d8b
feli <ftaurel@fi.uba.ar>
2024-06-17 14:31:18 -0300

    debugging?

--- server/src/index.js
+++ server/src/index.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://taurel-fitness-app.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,auth-token');
+  console.log(`Request received: ${req.method} ${req.url}`);
   next();
 });
 
