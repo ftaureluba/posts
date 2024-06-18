15c116d7a23acb1886bfa1ef5c44fbcbdab41725
feli <ftaurel@fi.uba.ar>
2024-06-17 21:32:45 -0300

    intentando solucionar un error con mongoose

--- server/api/index.js
+++ server/api/index.js
@@ -79,7 +79,7 @@ app.get("/api/posts", (req, res) => {
 });
 
 mongoose.connect(mongoDBURL)
-  //.then(() => {
+  .then(() => {
 
     async function findRutinas() {
       try {
@@ -174,11 +174,11 @@ mongoose.connect(mongoDBURL)
     });
     
     //findRutinas(); 
-  //})
-  /*
+  })
+  
   .catch((error) => {
     console.error('Error connecting to the database:', error);
-  });*/
+  });
 
 
 // app.js or routes file
