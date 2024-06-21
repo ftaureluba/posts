10ed414c696886dc296c9f29f78a0ffc31fee5f1
feli <ftaurel@fi.uba.ar>
2024-06-21 00:28:23 -0300

    el query a comparar intento pasarlo como objectId

--- server/api/index.js
+++ server/api/index.js
@@ -137,7 +137,7 @@ app.get("/api/posts", (req, res) => {
         const {mongoClient} = await mongoHandler();
         const db = mongoClient.db("Fitness-App")
         const collection = db.collection("rutinas")
-        const rutinaId = req.params.rutina_id;
+        const rutinaId = new ObjectId(req.params.rutina_id);
 
         // Verify if the provided ID is a valid MongoDB ObjectId
         if (!ObjectId.isValid(rutinaId)) {
