a283d6d54da36c6e7c25223a9495a7b130a18619
feli <ftaurel@fi.uba.ar>
2024-12-02 04:08:40 -0300

    rework en home, uso useMemo para que no se renderee las rutinas cuando se navega

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,15 @@
-/*import React from 'react'
-import Rutina from './Rutina'
 
-function Home() {
-  return (
-    <Rutina/>
-  )
-}*/
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Rutina from './Rutina';
 import { apiService } from '../services/PostsService';
 import '../styles/home.css';
-import { Link, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import AuthContext from './AuthContext';
 
 function Home ()  {
   const [rutinas, setRutinas] = useState([]);
   const navigate = useNavigate();
-  const {isLoggedIn, logout} = React.useContext(AuthContext);
+  const {isLoggedIn} = React.useContext(AuthContext);
   const handleNavigate = (path) => {
     navigate(path)
   };
@@ -24,11 +17,8 @@ function Home ()  {
   useEffect(() => {
     async function fetchRutinas ()  {
       try {
-        console.log("entro a fetchrutinas")
         const response = await apiService.fetchData('/api');
-        console.log('recibio response: ', response)
         const data = Array.isArray(response.data) ? response.data : [];
-        console.log('esta por setear las rutinas: ', response.data)
         setRutinas(data);
       } catch (error) {
         console.error('Error fetching rutinas:', error);
@@ -37,24 +27,31 @@ function Home ()  {
 
     fetchRutinas();
   }, []);
-  console.log(rutinas)
+
+  const rutinas_memo = useMemo(() => {
+    return rutinas.map(rutina => (
+      <div key={rutina._id} className='rutina-item'>
+        <Rutina key={rutina._id} rutina={rutina} />
+      </div>
+    ))
+  }, [rutinas])
+
+  const login_memo = useMemo(() => {
+    <div> 
+      <h1>Por favor, <button onClick={ () => handleNavigate('/login')}>inicie sesion</button> o <button onClick={ () => handleNavigate('/signup')}>crea una cuenta</button> para continuar </h1>
+    </div>
+  }, [handleNavigate])
   return (
     
     <div className='home-container'>
       {isLoggedIn ? (
         <div className='home-container'>
-      {Array.isArray(rutinas) && rutinas.map(rutina => (
-        <div key={rutina._id} className='rutina-item'>
-          <Rutina key={rutina._id} rutina={rutina} />
+          {rutinas_memo}
         </div>
-        
-      ))}
-      </div>)
-      :(
-        <div> <h1>Por favor, <button onClick={ () => handleNavigate('/login')}>inicie sesion</button> o <button onClick={ () => handleNavigate('/signup')}>crea una cuenta</button> para continuar </h1>
-         </div>
-      )
-    }
+      ):
+      (
+        login_memo
+      )}
     </div>
   );
 };
