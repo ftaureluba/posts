58369bc5b61ae4c8a1378750ab33c69a811cdabe
feli <ftaurel@fi.uba.ar>
2025-06-10 16:50:12 -0300

    check?

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { registerServiceWorker, setupInstallPrompt } from '../utils/pwa-utils';
+import { registerServiceWorker, setupInstallPrompt } from './utils/pwa-utils';
 
 
 
